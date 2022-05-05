a19c302cc306c93f1a7f8966668d301b0f35145b
abelrk <romer235@cougars.csusm.edu>
2022-05-05 15:44:17 -0700

    Add project files.

---
+++ SimplePassV19/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimplePassV19
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            bool supported = await Plugin.Fingerprint.CrossFingerprint.Current.IsAvailableAsync(true);
+            if (supported)
+            {
+                AuthenticationRequestConfiguration conf =
+                    new AuthenticationRequestConfiguration("Access our account", "Access your account");
+                var result = await CrossFingerprint.Current.AuthenticateAsync(conf);
+                if (result.Authenticated)
+                {
+                    await DisplayAlert("Success!", "Success", "ok");
+
+                }
+                else
+                {
+                    await DisplayAlert("Sorry", "Sorry", "Ok");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Sorry", "Sorry", "Ok");
+            }
+        }
+
+
+
+    }
+
+
+
+}
+
